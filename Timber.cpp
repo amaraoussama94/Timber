c79a8da84b21f285e2654733220ecdee6ccc6587
oussama.amara <wassa400@gmail.com>
2022-10-31 11:25:39 +0100

    creat the window 1920*1080

---
+++ Timber.cpp
@@ -0,0 +1,13 @@
+// Include important libraries here
+#include <SFML/Graphics.hpp>
+// Make code easier to type with "using namespace"
+using namespace sf;
+// This is where our game starts from
+int main()
+{
+// Create a video mode object
+VideoMode vm(1920, 1080);
+// Create and open a window for the game
+RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+return 0;
+}
